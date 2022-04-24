99dea24c5d9b9bbdf6ef473e58f10ba868ade56e
Vijay Ramakrishnan <vramak@microsoft.com>
2022-04-24 09:17:30 -0700

    Connecting the code to Redis

--- APIWithRedis/Program.cs
+++ APIWithRedis/Program.cs
@@ -1,7 +1,13 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDistributedRedisCache(option =>
+{
+    option.Configuration = builder.Configuration["RedisConnectionString"];
+});
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
@@ -12,5 +18,17 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello, World!");
+
+
+// Connecting to Redis
+var cache = ConnectionMultiplexer.Connect(builder.Configuration["RedisConnectionString"]).GetDatabase();
+
+// Redis usage
+var cacheKey = "ExecutionCount";
+var executionCount = (int)cache.StringGet(cacheKey);
+executionCount++;
+cache.StringSet(cacheKey, executionCount);
+
+app.MapGet("/Count", () => $"Execution count is: {executionCount}");
 
 app.Run();
